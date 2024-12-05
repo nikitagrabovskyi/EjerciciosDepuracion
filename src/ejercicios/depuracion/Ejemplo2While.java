93865b6b3e7f2a1e475acd90d9b69d0e9328e422
zmn22239 <zmn22239@educastur.es>
2024-12-05 04:10:19 +0100

    clase prueba reset

--- src/ejercicios/depuracion/Ejemplo2While.java
+++ src/ejercicios/depuracion/Ejemplo2While.java
@@ -11,9 +11,15 @@ public class Ejemplo2While {
         System.out.print("Introduce un número: ");
         n = sc.nextInt();
         while (contador< n){
-               System.out.println(" * ");
+               System.out.println("contador: "+contador);
                contador++;
-        }
+               
+        
     }
+        public static int pedirNumero() {
+     	   Scanner sc = new Scanner (System.in);
+     	   System.out.print("Introduce un numero:");
+     	   return sc nextInt();
+        }
 }
 
