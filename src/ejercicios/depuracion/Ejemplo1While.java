0e687cc2ad5b992b60aad43dca89162f81cd3064
zmn22239 <zmn22239@educastur.es>
2024-12-05 05:48:28 +0100

    atributos

--- src/ejercicios/depuracion/Ejemplo1While.java
+++ src/ejercicios/depuracion/Ejemplo1While.java
@@ -14,5 +14,12 @@ public class Ejemplo1While {
         }
         System.out.println("La suma es: " + suma );
     }
+    private int suma, num;
+
+    public Ejemplo1While() {
+
+    this.suma = 0;
+
+    this.num = 0;
 }
 
